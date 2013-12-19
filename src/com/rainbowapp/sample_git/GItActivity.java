6ac681344128da9141e9224f4f48c10add75c8f9
Takuya Iwasaki <iwasakitakuya19861111@gmail.com>
2013-12-19 20:35:06 +0900

    GitActivityというClassを追加したよ。

---
+++ src/com/rainbowapp/sample_git/GItActivity.java
@@ -0,0 +1,13 @@
+package com.rainbowapp.sample_git;
+
+public class GItActivity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
